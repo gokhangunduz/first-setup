0a29a50e271d793f7ceec763ddbce10564e1a6e4
gokhangunduz <me@gokhangunduz.com.tr>
2024-01-12 13:23:28 +0300

    refactor(vs-code.sh): :tada: update vs-code.deb name

--- installations/vs-code.sh
+++ installations/vs-code.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo wget 'https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64' -O ./vscode.deb
-sudo apt install ./vscode.deb -y
+sudo wget 'https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64' -O ./vs-code.deb
+sudo apt install ./vs-code.deb -y
