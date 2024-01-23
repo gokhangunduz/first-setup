9b80f1a49bfb49a3f343673d16456ef644aa9924
gokhangunduz <me@gokhangunduz.com.tr>
2024-01-23 23:17:55 +0300

    refactor(setup.sh): :tada: update dialog handler

--- packages/docker.sh
+++ packages/docker.sh
@@ -19,4 +19,4 @@ sudo apt-get update
 
 # Install Docker with plugins<
 export DEBIAN_FRONTEND=noninteractive
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
