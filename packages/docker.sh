194bc19e19305277d82eb2766716aa5820a82472
gokhangunduz <me@gokhangunduz.com.tr>
2024-01-23 23:14:17 +0300

    refactor(setup.sh): :tada: update dialog handler

--- packages/docker.sh
+++ packages/docker.sh
@@ -6,7 +6,7 @@ export DEBIAN_FRONTEND=noninteractive
 # Add Docker's official GPG key:
 sudo apt-get install ca-certificates curl gnupg -y
 sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
 # Add the repository to apt sources:
@@ -14,12 +14,9 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Update apt package index:
 sudo apt-get update
 
 # Install Docker with plugins
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-# Add user to docker group
-sudo groupadd docker
-sudo usermod -aG docker $USER
-newgrp docker
