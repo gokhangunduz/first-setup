25209fa8c0559b2ca6b9a160e2d2bcf93c4deb99
gokhangunduz <me@gokhangunduz.com.tr>
2024-01-23 23:15:02 +0300

    refactor(setup.sh): :tada: update dialog handler

--- packages/docker.sh
+++ packages/docker.sh
@@ -6,7 +6,7 @@ export DEBIAN_FRONTEND=noninteractive
 # Add Docker's official GPG key:
 sudo apt-get install ca-certificates curl gnupg -y
 sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
 # Add the repository to apt sources:
