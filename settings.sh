979e9b3ff7ab606c667210dec46f74aef0440389
Gökhan Gündüz <me@gokhangunduz.com.tr>
2024-01-27 16:39:01 +0300

    refactor(setup.sh): 🎉 update setup.sh

--- settings.sh
+++ settings.sh
@@ -3,6 +3,7 @@
 echo "System is preparing..."
 
 # Dock
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-position 'RIGHT'
 gsettings set org.gnome.shell.extensions.dash-to-dock autohide true
 gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 30
 gsettings set org.gnome.nautilus.icon-view default-zoom-level 'small'
